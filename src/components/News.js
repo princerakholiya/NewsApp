edc24f9ab5566cdc3ac9022351e3cbd93eaadfb9
princerakholiya2233 <prince.rakholiya@admybrand.com>
2023-07-31 18:20:48 +0530

    updated

--- src/components/News.js
+++ src/components/News.js
@@ -63,7 +63,7 @@ const News = (props) => {
 
     return (
         <div className="container my-3">
-            <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+            <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsApp - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
             {loading && <Spinner />}
             <InfiniteScroll
                 dataLength={articles.length}
